9c1e03fbfb851186443b063c8d2682d6e4d8580a
LeoSole423 <leonel.sole423@gmail.com>
2021-07-26 13:17:57 -0300

    user model

---
+++ api/controllers/UserController.js
@@ -0,0 +1,30 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  login: async function (req, res) {
+    let email_taked = req.param('email');
+    let password_taked = req.param('password');
+
+    const user = await User.findOne({
+      email: email_taked,
+    });
+    if (user && sails.argon2.verify(user.password, contra)) {
+      req.session.user = user;
+      res.redirect('/');
+    }
+    else {
+      req.session.user = null;
+      res.view('pages/login');
+    }
+  },
+  logout: async function (req, res){
+    req.session.user = null;
+    res.redirect ('/');
+  }
+};
+
